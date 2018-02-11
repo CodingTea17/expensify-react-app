b44c96bbffb025499ee0aaccce0f31f4e7fa182e
Dawson M <dawson.mortenson@gmail.com>
2018-02-11 11:52:41 -0800

    add a couple more test-tests

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,4 +1,5 @@
 const add = (a, b) => a + b;
+const generateGreeting = (name = "Anon") => `Hello, ${name}!`
 
 test('should add two numbers together', () => {
   const result = add(3, 4);
@@ -7,4 +8,14 @@ test('should add two numbers together', () => {
   //   throw new Error(`4 plus 3 is not ${result}. Expect 7.`)
   // }
   expect(result).toBe(7);
+});
+
+test('should return an interpolated greeting from a name', () => {
+  const result = generateGreeting("Bob");
+  expect(result).toBe("Hello, Bob!");
+});
+
+test('should return an interpolated greeting when there is no name', () => {
+  const result = generateGreeting();
+  expect(result).toBe("Hello, Anon!");
 });
