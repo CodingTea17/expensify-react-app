a447d19063847b0b557eb89c88e8639d48935ce6
Dawson M <dawson.mortenson@gmail.com>
2018-02-11 12:25:53 -0800

    add tests for edit and add expense actions

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,3 +7,41 @@ test('should trigger remove expense action object', () => {
     id: '12345'
   });
 });
+
+test('should trigger edit expense action object', () => {
+  const action = editExpense( '12345' , { note: 'truckload of bananas' });
+  expect(action).toEqual({
+    type: 'EDIT_EXPENSE',
+    id: '12345',
+    updates: { note: 'truckload of bananas' }
+  });
+});
+
+test('should trigger the add expense action object', () => {
+  const newExpense = {
+    description: 'Chocolate',
+    note: 'It was worth it.',
+    amount: 1000,
+    createdAt: 90000
+  };
+  const action = addExpense(newExpense);
+
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: { ...newExpense, id: expect.any(String) }
+  })
+});
+
+test('should trigger the add expense action object with default values', () => {
+  const action = addExpense()
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0,
+      id: expect.any(String)
+    }
+  });
+});
